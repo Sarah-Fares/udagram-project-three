fe8fcb4c66df3f43a80d3d232517579cd488c40b
Sarah-Fares <sarah.fares.dev@gmail.com>
2022-11-24 08:37:28 +0200

    edits set_env.sh

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+# trace
+export FWD_POSTGRES_USERNAME=postgres
+export FWD_POSTGRES_PASSWORD=postgres
+export FWD_POSTGRES_HOST=udagram-proj3-db.cik9rggykbgd.us-east-1.rds.amazonaws.com
+export FWD_POSTGRES_DB=postgres
+export FWD_AWS_BUCKET=project3-32324651708-dev
+export FWD_AWS_REGION=us-east-1
+export FWD_AWS_PROFILE=default
+export FWD_JWT_SECRET=testing
+export FWD_URL=http://ac3ecf546296b40bfa13912c55715832-1961231541.us-east-1.elb.amazonaws.com/
